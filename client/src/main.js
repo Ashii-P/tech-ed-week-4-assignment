90b0356fc196565b7f7434e14a3a3668673f62ae
Aishah Perks <byaishah.altaf@gmail.com>
2025-02-02 16:42:27 +0000

    added some client js

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const app = document.getElementById("app");
+const cymru_api = "https://tech-ed-week-4-assignment.onrender.com";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function fetchPosts() {
+  const response = await fetch(`${cymru_api}/cymruposts`);
+  const apiData = await response.json();
+  displayCymruPosts(apiData);
+}
+function displayCymruPosts(param) {
+  param.forEach((singlePost) => {
+    const h3 = document.createElement(`h3`);
+    const h4 = document.createElement(`h4`);
+    const pTag = document.createElement(`p`);
+    const image = document.createElement(`IMG`);
+    const deleteButton = document.createElement(`button`);
+    const div = document.createElement("div");
 
-setupCounter(document.querySelector('#counter'))
+    h3.innerText = singlePost.place;
+    h4.innerText = singlePost.postcode;
+    pTag.innerText = singlePost.comment;
+    image.src = singlePost.photo;
+    deleteButton.innerText = `Delete Post`;
+
+    deleteButton.addEventListener(`click`, () => {
+      handleDelete(singlePost.id);
+    });
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(pTag);
+    div.appendChild(image);
+    div.appendChild(deleteButton);
+
+    app.appendChild(div);
+  });
+}
+
+fetchPosts();
+async function handleDelete(id) {
+  const response = await fetch(`${cymru_api}/cymruposts/${id}`, {
+    method: `DELETE`,
+  });
+  if (response.ok) {
+    fetchPosts();
+  }
+}
