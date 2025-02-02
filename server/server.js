f7f87c5b385e6893eba54e914cfa7bcfe7fcb80e
Aishah Perks <byaishah.altaf@gmail.com>
2025-02-02 16:02:20 +0000

    initial commit

--- server/server.js
+++ server/server.js
@@ -13,15 +13,16 @@ const dataBase = new pg.Pool({
   connectionString: process.env.DB_CONN,
 });
 
+app.get("/", (req, res) => res.json("hello?"));
 app.get("/cymruposts", async (req, res) => {
-  const result = await dataBase.query(`SELECT * FROM cymruposts`);
+  const result = await dataBase.query("SELECT * FROM cymruposts");
   const posts = result.rows;
   res.json(posts);
 });
 
 app.delete("/cymruposts/:id", async (req, res) => {
   console.log(req.params.id);
-  const deleted = await dataBase.query(`DELETE FROM cymruposts WHERE id = $1`, [
+  const deleted = await dataBase.query("DELETE FROM cymruposts WHERE id = $1", [
     req.params.id,
   ]);
   res.send(req.params.id);
@@ -29,7 +30,7 @@ app.delete("/cymruposts/:id", async (req, res) => {
 app.put("/cymruposts/:id", async (req, res) => {
   console.log(req.params.id, req.body);
   const update = await dataBase.query(
-    `UPDATE cymruposts SET place = $1, postcode = $2, comment = $3, photo = $4 WHERE id = $5`,
+    "UPDATE cymruposts SET place = $1, postcode = $2, comment = $3, photo = $4 WHERE id = $5",
     [req.body.place, req.body.postcode, req.body.comment, req.body.photo]
   );
   res.json({ params: req.params.id, body: req.body });
@@ -45,12 +46,12 @@ app.post("/cymruposts", async (req, res) => {
   const photoFromClient = req.body.photo;
 
   const newData = await dataBase.query(
-    `INSERT INTO cymruposts (place, postcode, comment, photo) VALUES ($1, $2, $3, $4)`,
+    "INSERT INTO cymruposts (place, postcode, comment, photo) VALUES ($1, $2, $3, $4)",
     [placeFromClient, postcodeFromClient, commentFromClient, photoFromClient]
   );
   res.json(newData);
 });
 
-app.listen("2098", () => {
-  console.log(`App is running on http://localhost:2098`);
+app.listen("3098", () => {
+  console.log(`App is running on http://localhost:3098`);
 });
